c3be111d17b8304bb190fd47c2cd6087835d1614
Siddharth Shekar <sshekar@ee.columbia.edu>
2018-08-08 23:11:00 -0400

    Allow for creating and posting only update zips

--- setup.py
+++ setup.py
@@ -10,103 +10,114 @@ import shutil
 import sys
 from cx_Freeze import setup, Executable
 
+build = False
 post = False
+update = False
+if 'build' in sys.argv:
+    sys.argv.remove('build')
+    build = True
+
 if 'post' in sys.argv:
     sys.argv.remove('post')
     post = True
 
-base = None
-if sys.platform == 'win32':
-    base = 'Win32GUI'
+if 'update' in sys.argv:
+    sys.argv.remove('update')
+    update = True
 
-includes = [
-    'copy',
-    'ctypes',
-    'distutils',
-    'io',
-    'os',
-    'pickle',
-    'platform',
-    'PyQt5',
-    'sip',
-    'sympy',
-    'src',
-    'sys']
+if build is True:
+    base = None
+    if sys.platform == 'win32':
+        base = 'Win32GUI'
 
-include_files = [
-    'Resources'
-]
+    includes = [
+        'copy',
+        'ctypes',
+        'distutils',
+        'io',
+        'os',
+        'pickle',
+        'platform',
+        'PyQt5',
+        'sip',
+        'sympy',
+        'src',
+        'sys']
 
-# Need to include imageformats as per
-# https://stackoverflow.com/a/5722333
-if sys.platform == 'win32':
-    include_files.append(sys.prefix + '/Library/plugins/imageformats')
-    include_files.append(sys.prefix + '/Library/plugins/platforms')
-    include_files.append(sys.prefix + '/Library/bin/libeay32.dll')
-    include_files.append(sys.prefix + '/Library/bin/ssleay32.dll')
-if sys.platform == 'linux':
-    include_files.append(sys.prefix + '/lib/x86_64-linux-gnu/qt5/plugins/imageformats')
-    include_files.append(sys.prefix + '/lib/x86_64-linux-gnu/qt5/plugins/platforms')
-    # include_files.append(sys.prefix + '/lib/x86_64-linux-gnu/libQt5Svg.so.5')
-include_files.append('./LICENSE.txt')
+    include_files = [
+        'Resources'
+    ]
 
-excludes = [
-    'concurrent',
-    'curses',
-    'email',
-    'html',
-    'http',
-    'IPython',
-    'json'
-    'jupyter',
-    'lib2to3',
-    'lxml',
-    'matplotlib',
-    'mpl_toolkits',
-    'multiprocessing',
-    'nose',
-    'numpy',
-    'PIL',
-    'py',
-    'pydoc_data',
-    'scipy',
-    'tkinter',
-    'urllib',
-    'xml'
-]
+    # Need to include imageformats as per
+    # https://stackoverflow.com/a/5722333
+    if sys.platform == 'win32':
+        include_files.append(sys.prefix + '/Library/plugins/imageformats')
+        include_files.append(sys.prefix + '/Library/plugins/platforms')
+        include_files.append(sys.prefix + '/Library/bin/libeay32.dll')
+        include_files.append(sys.prefix + '/Library/bin/ssleay32.dll')
+    if sys.platform == 'linux':
+        include_files.append(sys.prefix + '/lib/x86_64-linux-gnu/qt5/plugins/imageformats')
+        include_files.append(sys.prefix + '/lib/x86_64-linux-gnu/qt5/plugins/platforms')
+        # include_files.append(sys.prefix + '/lib/x86_64-linux-gnu/libQt5Svg.so.5')
+    include_files.append('./LICENSE.txt')
 
-options = {
-    'build_exe': {
-        'includes': includes,
-        'excludes': excludes,
-        'include_files': include_files,
-        'zip_include_packages': [x for x in includes if x != 'src'],
-        'optimize': 2
+    excludes = [
+        'concurrent',
+        'curses',
+        'email',
+        'html',
+        'http',
+        'IPython',
+        'json'
+        'jupyter',
+        'lib2to3',
+        'lxml',
+        'matplotlib',
+        'mpl_toolkits',
+        'multiprocessing',
+        'nose',
+        'numpy',
+        'PIL',
+        'py',
+        'pydoc_data',
+        'scipy',
+        'tkinter',
+        'urllib',
+        'xml'
+    ]
+
+    options = {
+        'build_exe': {
+            'includes': includes,
+            'excludes': excludes,
+            'include_files': include_files,
+            'zip_include_packages': [x for x in includes if x != 'src'],
+            'optimize': 2
+        }
     }
-}
 
-targetName = 'YCircuit'
-if sys.platform == 'win32':
-    targetName += '.exe'
+    targetName = 'YCircuit'
+    if sys.platform == 'win32':
+        targetName += '.exe'
 
-executables = [
-    Executable('top.py',
-               base=base,
-               targetName=targetName
-    )
-]
+    executables = [
+        Executable('top.py',
+                base=base,
+                targetName=targetName
+        )
+    ]
 
-# Remove build directory if it exists
-if os.path.isdir('build'):
-    print('Removing build')
-    shutil.rmtree('build')
+    # Remove build directory if it exists
+    if os.path.isdir('build'):
+        print('Removing build')
+        shutil.rmtree('build')
 
-setup(name='YCircuit',
-      version='0.3',
-      description='YCircuit',
-      options=options,
-      executables=executables
-      )
+    setup(name='YCircuit',
+        version='0.3',
+        description='YCircuit',
+        options=options,
+        executables=executables
+        )
 
 # Get current branch name
 from subprocess import check_output
@@ -118,10 +129,11 @@ version = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
 
 if sys.platform == 'win32':
     import zipfile
-    with zipfile.ZipFile('build/ycircuit-' + branch + '_win64.zip', 'w', zipfile.ZIP_DEFLATED) as zip:
-        for root, dirs, files in os.walk('build/exe.win-amd64-' + version):
-            for file in files:
-                zip.write(os.path.join(root, file))
+    if build is True:
+        with zipfile.ZipFile('build/ycircuit-' + branch + '_win64.zip', 'w', zipfile.ZIP_DEFLATED) as zip:
+            for root, dirs, files in os.walk('build/exe.win-amd64-' + version):
+                for file in files:
+                    zip.write(os.path.join(root, file))
     with zipfile.ZipFile('build/ycircuit-' + branch + '_win64_update.zip', 'w', zipfile.ZIP_DEFLATED) as zip:
         zip.write('build/exe.win-amd64-' + version + '/YCircuit.exe', 'YCircuit.exe')
         for root, dirs, files in os.walk('src'):
@@ -133,19 +145,23 @@ if sys.platform == 'win32':
     if post is True:
         # Note that BB_AUTH_STRING must be set to the right value as an env variable
         from subprocess import call
-        call(['curl',
+        cmd = ['curl',
               '-s',
               '-u', os.environ['BB_AUTH_STRING'],
               '-X', 'POST',
-              'https://api.bitbucket.org/2.0/repositories/siddharthshekar/ycircuit/downloads',
-              '-F', 'files=@build/ycircuit-' + branch + '_win64.zip',
-              '-F', 'files=@build/ycircuit-' + branch + '_win64_update.zip'])
+              'https://api.bitbucket.org/2.0/repositories/siddharthshekar/ycircuit/downloads']
+        if build is True:
+            cmd += ['-F', 'files=@build/ycircuit-' + branch + '_win64.zip']
+        if update is True:
+            cmd += ['-F', 'files=@build/ycircuit-' + branch + '_win64_update.zip']
+        call(cmd)
 if sys.platform == 'linux':
     import zipfile
-    with zipfile.ZipFile('build/ycircuit-' + branch + '_linux64.zip', 'w', zipfile.ZIP_DEFLATED) as zip:
-        for root, dirs, files in os.walk('build/exe.linux-x86_64-' + version):
-            for file in files:
-                zip.write(os.path.join(root, file))
+    if build is True:
+        with zipfile.ZipFile('build/ycircuit-' + branch + '_linux64.zip', 'w', zipfile.ZIP_DEFLATED) as zip:
+            for root, dirs, files in os.walk('build/exe.linux-x86_64-' + version):
+                for file in files:
+                    zip.write(os.path.join(root, file))
     with zipfile.ZipFile('build/ycircuit-' + branch + '_linux64_update.zip', 'w', zipfile.ZIP_DEFLATED) as zip:
         zip.write('build/exe.linux-x86_64-' + version + '/YCircuit', 'YCircuit')
         for root, dirs, files in os.walk('src'):
@@ -160,10 +176,13 @@ if sys.platform == 'linux':
     if post is True:
         # Note that BB_AUTH_STRING must be set to the right value as an env variable
         from subprocess import call
-        call(['curl',
+        cmd = ['curl',
               '-s',
               '-u', os.environ['BB_AUTH_STRING'],
               '-X', 'POST',
-              'https://api.bitbucket.org/2.0/repositories/siddharthshekar/ycircuit/downloads',
-              '-F', 'files=@build/ycircuit-' + branch + '_linux64.zip',
-              '-F', 'files=@build/ycircuit-' + branch + '_linux64_update.zip'])
+              'https://api.bitbucket.org/2.0/repositories/siddharthshekar/ycircuit/downloads']
+        if build is True:
+            cmd += ['-F', 'files=@build/ycircuit-' + branch + '_linux64.zip']
+        if update is True:
+            cmd += ['-F', 'files=@build/ycircuit-' + branch + '_linux64_update.zip']
+        call(cmd)
